a862602c52c44939c6f464e6a2494b756d262a31
NTDELLMARUSKA\Marie <maruspondeli@gmail.com>
2021-04-20 19:27:59 +0200

    farma - kráva je tam

--- 01-majitelka-vozu/app.js
+++ 01-majitelka-vozu/app.js
@@ -1,10 +1,32 @@
 // Vytvoř objekt auto, který bude mít vlastnosti: znacka, barva, rokVyroby,..
-
+let auto = {
+    znacka: 'Ford',
+    barva: 'modrá',
+    rokVyroby: 2015,
+    spz: '1A11111',
+};
 
 // Přidej novou vlastnost (majitel a prirad ji svoje jmeno jako hodnotu)
+auto.majitel = {};
+auto.majitel.jmeno = 'Jana';
+auto.majitel.prijmeni = 'Czechita'
 
 
 // Přepiš značku na Škoda
-
-
+auto.znacka = 'Škoda';
+ console.log(auto)
 // Vytvor tri promenne majitelka, znacka a spz a vloz do techto elementu text z objektu auto.
+const majitelka = auto.majitel.jmeno + ' ' + auto.majitel.prijmeni;
+document.querySelector('#majitelka').innerHTML = majitelka
+
+const znacka = auto.znacka;
+document.querySelector('#znacka').innerHTML = znacka;
+
+const spz = auto.spz;
+document.querySelector('#spz').innerHTML = spz;
+
+//apendCHild
+
+let odstavec = document.createElement('p');
+odstavec.innerText = "Ahoj, odstavec"
+document.querySelector('#spz').appendChild(odstavec);
